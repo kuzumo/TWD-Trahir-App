542fa63bf467564f17a7589cd27e9b09a7ceb05b
JM <sumampongjeyem@gmail.com>
2024-10-20 09:29:56 +0800

    added functions
    I added functions for next buttons and end rounds

---
+++ Trahir App/Assets/MainMenu.cs
@@ -0,0 +1,181 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame() {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void EndRound() {
+        SceneManager.LoadSceneAsync(41);
+    }
+
+    public void DrawRound() {
+        SceneManager.LoadSceneAsync(42);
+    }
+
+    public void PlayAgain() {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void NextRound2() {
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void NextRound3() {
+        SceneManager.LoadSceneAsync(3);
+    }
+
+    public void NextRound4() {
+        SceneManager.LoadSceneAsync(4);
+    }
+
+    public void NextRound5() {
+        SceneManager.LoadSceneAsync(5);
+    }
+
+    public void NextRound6() {
+        SceneManager.LoadSceneAsync(6);
+    }
+
+    public void NextRound7() {
+        SceneManager.LoadSceneAsync(7);
+    }
+
+    public void NextRound8() {
+        SceneManager.LoadSceneAsync(8);
+    }
+
+    public void NextRound9() {
+        SceneManager.LoadSceneAsync(9);
+    }
+
+    public void NextRound10() {
+        SceneManager.LoadSceneAsync(10);
+    }
+
+    public void NextRound11() {
+        SceneManager.LoadSceneAsync(11);
+    }
+
+    public void NextRound12() {
+        SceneManager.LoadSceneAsync(12);
+    }
+
+    public void NextRound13() {
+        SceneManager.LoadSceneAsync(13);
+    }
+
+    public void NextRound14() {
+        SceneManager.LoadSceneAsync(14);
+    }
+
+    public void NextRound15() {
+        SceneManager.LoadSceneAsync(15);
+    }
+
+    public void NextRound16() {
+        SceneManager.LoadSceneAsync(16);
+    }
+
+    public void NextRound17() {
+        SceneManager.LoadSceneAsync(17);
+    }
+
+    public void NextRound18() {
+        SceneManager.LoadSceneAsync(18);
+    }
+
+    public void NextRound19() {
+        SceneManager.LoadSceneAsync(19);
+    }
+
+    public void NextRound20() {
+        SceneManager.LoadSceneAsync(20);
+    }
+
+    public void NextRound21() {
+        SceneManager.LoadSceneAsync(21);
+    }
+
+    public void NextRound22() {
+        SceneManager.LoadSceneAsync(22);
+    }
+
+    public void NextRound23() {
+        SceneManager.LoadSceneAsync(23);
+    }
+
+    public void NextRound24() {
+        SceneManager.LoadSceneAsync(24);
+    }
+
+    public void NextRound25() {
+        SceneManager.LoadSceneAsync(25);
+    }
+
+    public void NextRound26() {
+        SceneManager.LoadSceneAsync(26);
+    }
+
+    public void NextRound27() {
+        SceneManager.LoadSceneAsync(27);
+    }
+
+    public void NextRound28() {
+        SceneManager.LoadSceneAsync(28);
+    }
+
+    public void NextRound29() {
+        SceneManager.LoadSceneAsync(29);
+    }
+
+    public void NextRound30() {
+        SceneManager.LoadSceneAsync(30);
+    }
+
+    public void NextRound31() {
+        SceneManager.LoadSceneAsync(31);
+    }
+
+    public void NextRound32() {
+        SceneManager.LoadSceneAsync(32);
+    }
+
+    public void NextRound33() {
+        SceneManager.LoadSceneAsync(33);
+    }
+
+    public void NextRound34() {
+        SceneManager.LoadSceneAsync(34);
+    }
+
+    public void NextRound35() {
+        SceneManager.LoadSceneAsync(35);
+    }
+
+    public void NextRound36() {
+        SceneManager.LoadSceneAsync(36);
+    }
+
+    public void NextRound37() {
+        SceneManager.LoadSceneAsync(37);
+    }
+
+    public void NextRound38() {
+        SceneManager.LoadSceneAsync(38);
+    }
+
+    public void NextRound39() {
+        SceneManager.LoadSceneAsync(39);
+    }
+
+    public void NextRound40() {
+        SceneManager.LoadSceneAsync(40);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
